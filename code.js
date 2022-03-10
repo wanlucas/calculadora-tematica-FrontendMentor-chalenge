4a874b0f4d1ed51c30052fcf7aeb937f7302140e
wan <wan.lucas.90@gmail.com>
2022-03-10 17:23:10 -0300

    versao final

--- code.js
+++ code.js
@@ -1,30 +1,53 @@
-
-
+const bodyHtml = document.body;
 const themeHtml = document.querySelector('.themes-options');
 const displayHtml = document.querySelector('.display');
-const keysHTML = document.querySelectorAll('.key');
 
-var actTheme = 1;
+var actTheme = 1, calc = '';
 
-themeHtml.addEventListener("click", () => {
+themeHtml.addEventListener("click", ()=>{
     actTheme == 3 ? actTheme = 1 : actTheme++;
     updateTheme(actTheme);
 })
-keysHTML.forEach(key => {
-    key.addEventListener('click', ()=> {
-        updateCalc(key.innerText);
+function updateTheme(theme) {
+        switch(theme) {
+            case 1 : {
+                bodyHtml.setAttribute('data-theme', 'theme-primary');
+                themeHtml.style.justifyContent = 'left';
+            }; break;
+            case 2 : {
+                bodyHtml.setAttribute('data-theme', 'theme-secundary');
+                themeHtml.style.justifyContent = 'center';
+            }; break;
+            case 3 : {
+                bodyHtml.setAttribute('data-theme', 'theme-tertiary');
+                themeHtml.style.justifyContent = 'right';
+            }; break;
+        }
+}
+document.querySelectorAll('.key').forEach(key => {
+    key.addEventListener('click', ()=>{
+        const d = displayHtml, k = key.innerText;
+        if(d.innerText.length > 10 || k == 0) return;
+        if(!/[0-9]/.test(d.innerText.split('').pop()) && !/[0-9]/.test(k)) del();
+        d.innerText == '0' ? d.innerText = k : d.innerText += k;
+        if(k == 'x') return calc+= '*'; 
+        calc += k;
+        console.log(calc)
     });
 });
-
-function updateTheme(theme) {
-     themeHtml.style.animation = '';
-     setTimeout(()=>{
-        switch(theme) {
-        }
-    }, 40)
+document.querySelector('#reset').addEventListener("click", ()=>{
+    displayHtml.innerText='0', calc = '';
+})
+document.querySelector('#delete').addEventListener("click", del);
+function del() {
+    const txt = displayHtml.innerText.split('');
+    let res =  txt.slice(0, txt.length-1).join('');
+    if(res == '') {return displayHtml.innerText='0', calc=''};
+    displayHtml.innerText = res, calc = res;
+}
+document.querySelector('#calc').onclick = ()=> {
+    res = eval(calc);
+    Number.isInteger(res) ? 0 : res = res.toFixed(2);
+    displayHtml.innerText = res, calc = res;
 }
 
-
-function updateCalc(key) {
-    displayHtml.innerHTML += key
-}
